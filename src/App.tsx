c5c222df01aacd2ac17e17b8a6d71650ef39e9dd
76351506@163.com <76351506@163.com>
2020-10-21 09:10:22 +0800

    'heinan'

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 /*
- * @Author: heinan 
- * @Date: 2020-01-09 10:00:18 
- * @Last Modified by:   heinan 
- * @Last Modified time: 2020-01-09 10:00:18 
+ * @Author: heinan
+ * @Date: 2020-01-09 10:00:18
+ * @Last Modified by:   heinan
+ * @Last Modified time: 2020-01-09 10:00:18
  */
-import React from 'react';
+import React, { Suspense } from 'react';
 import RouterView from '@/router';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const App: React.FC = function() {
+const App: React.FC = function () {
   return (
-    <Router>
-      <RouterView></RouterView>
-    </Router>
+    <Suspense fallback={<div>...loading</div>}>
+      <Router>
+        <RouterView></RouterView>
+      </Router>
+    </Suspense>
   );
 };
 export default App;
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,15 +1,16 @@
-import Home from '@/containers/home';
-import Login from '@/containers/login';
+import { lazy } from 'react';
+const Home = lazy(() => import('@/containers/home'));
+const Login = lazy(() => import('@/containers/login'));
 
 export default [
   {
     path: '/home',
     name: 'home',
-    component: Home
+    component: Home,
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login
-  }
+    component: Login,
+  },
 ];
